0d8cfa50d4ed1a4431885bf743cf16a13c8ec39a
EslebanPerez <eslebanperez@gmail.com>
2022-04-25 14:58:31 -0500

    Agregando la propiedad de prueba

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -2,7 +2,7 @@ const MissionCommander = require('./../app/missionCommander') //Importando Clase
 
 describe("Unit test for Mission Commander Class", () => {
     test('1) Create mission comander object', () => {
-        const myMissionCommander = new MissionCommander()
-        expect(result).toBe(3);
+        const myMissionCommander = new MissionCommander('Woopa')
+        expect(myMissionCommander.name).toBe('Woopa');
     });
 })
